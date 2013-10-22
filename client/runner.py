69eb51c5c58905e7985ce3105225a8f4c82c6f9a
Z.centosVM <liuzitian1@gmail.com>
2013-10-21 20:33:05 -0700

    minor fixes

--- client/runner.py
+++ client/runner.py
@@ -2,23 +2,18 @@ import os
 import sys
 import time
 import random
+import tempfile
 import subprocess
 from datetime import datetime
 
 cmd = "/usr/bin/wget --timeout=8 -e robots=off -U \"Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0\" --page-requisites --no-check-certificate "
-replicationCap = 7
-iterations = 100
+iterations = 3
 
 class Trail:
-    def __init__(self,website):
+    def __init__(self,website,reps):
         self.command = cmd+" "+website+" &>/dev/null"
-        randBool = bool(random.getrandbits(1))
-        self.ifReplicate = randBool
-        if(randBool):
-            self.numReps = random.randint(1,replicationCap)
-        else:
-            self.numReps = 0
-        self.waitTime = 10.0 * random.random()+0.01
+        self.numReps = reps
+        self.waitTime = 10*random.random()+0.1
         self.website = website
 
 def getDefaultResolver():
@@ -45,18 +40,20 @@ def setResolver(resolver):
         print "failed to set resolver to "+str(resolver)
         exit(1)
 
-def getRandomDnsList(lst,numNeeded):
+def getRandomDnsList(defaultDns,lst,numNeeded):
     if(numNeeded > len(lst)):
         numNeeded = len(lst)
     ret = []
     random.shuffle(lst)
-    for i in range(numNeeded):
+    ret.append(defaultDns)
+    for i in range(numNeeded-1):
         ret.append(lst[i])
     return ret
 
 def writeResult(trail,time,fileHandle):
-    string = str(trail.numReps)+","+str(trail.website)+","+time
+    string = str(trail.numReps)+","+str(trail.website)+","+str(time)
     fileHandle.write(string+"\n")
+    os.fsync(fileHandle)
 ###################################################################################
 
 def main():
@@ -71,56 +68,75 @@ def main():
     proxyBin = sys.argv[1]
     dnsListFile = open(sys.argv[2])
     domainListFile = open(sys.argv[3])
-    resultFile = open("result.csv","w+")
+    resultFile = open("result.csv","w")
     defaultResolver = getDefaultResolver()
     allDomains = []
     allTrails = []
     allDnsServers = []
 
+    DEVNULL = open(os.devnull,'wb')
+
     print "using "+defaultResolver+" as default resolver"
 
     for thisDomain in domainListFile:
-        allDomains.append(thisDomain)
+        allDomains.append(thisDomain.rstrip())
 
     for thisServer in dnsListFile:
-        allDnsServers.append(thisServer)
+        allDnsServers.append(thisServer.rstrip())
 
     dnsListFile.close()
     domainListFile.close()
 
-    dup = allDomains[:]
     for i in range(iterations):
-        allDomains = allDomans + dup
-
-    for i in allDomains:
-        allTrails.append(Trail(i))
+        for d in allDomains:
+            allTrails.append(Trail(d,1))
+        for d in allDomains:
+            allTrails.append(Trail(d,1))
+        for d in allDomains:
+            allTrails.append(Trail(d,1))
+        for d in allDomains:
+            allTrails.append(Trail(d,1))
+        for d in allDomains:
+            allTrails.append(Trail(d,1))
+        for d in allDomains:
+            allTrails.append(Trail(d,2))
+        for d in allDomains:
+            allTrails.append(Trail(d,3))
+        for d in allDomains:
+            allTrails.append(Trail(d,4))
+        for d in allDomains:
+            allTrails.append(Trail(d,5))
+        for d in allDomains:
+            allTrails.append(Trail(d,6))
 
     random.shuffle(allTrails)
 
     for trail in allTrails:
-        sleep(trail.waitTime)
-        if(trail.ifReplicate):
+        time.sleep(trail.waitTime)
+        if(trail.numReps != 1):
             tempDNSFile = tempfile.NamedTemporaryFile(delete=False)
-            dnsList = getRandomDnsList(allDnsServers,trail.numReps)
+            dnsList = getRandomDnsList(defaultResolver,allDnsServers,trail.numReps)
             for i in dnsList:
                 tempDNSFile.write(i+"\n")
             tempDNSFile.close()
 
-            proxy = subprocess.Popen([proxyBin,'-f',tempDNSFile.name])
+            proxy = subprocess.Popen([proxyBin,'-f',tempDNSFile.name], stdout=DEVNULL, stderr=DEVNULL)
             setResolver("127.0.0.1")
-            sleep(0.1)
-            runtime = timedExecute(trail.command)
+            time.sleep(0.5)
+            runtime = timedExecuteMicroSecond(trail.command)
             assert(proxy.returncode == None)
             writeResult(trail,runtime,resultFile)
-            proxy.send_signal(SIGQUIT)
-            os.remove(tempDNSFile.name)
+            proxy.send_signal(3) #sigquit
+            proxy.wait()
+            #os.remove(tempDNSFile.name)
         else:
             setResolver(defaultResolver)
-            runtime = timedExecute(trail.command)
+            runtime = timedExecuteMicroSecond(trail.command)
             writeResult(trail,runtime,resultFile)
 
     resultFile.close()
     setResolver(defaultResolver)
+    DEVNULL.close()
     
     
 if(__name__ == '__main__'):
